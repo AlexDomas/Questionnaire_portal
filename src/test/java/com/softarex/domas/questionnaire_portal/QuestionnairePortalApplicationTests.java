4acc2e4d5739ccbd8526df87990e51a436a4f8ca
AlexDomas <aleksej.domas@mail.ru>
2022-05-30 22:58:14 +0300

    init commit

---
+++ src/test/java/com/softarex/domas/questionnaire_portal/QuestionnairePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softarex.domas.questionnaire_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnairePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
