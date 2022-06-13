d5ec0825ab33a55b2729bf7a6818a69cd9c2720b
AlexDomas <aleksej.domas@mail.ru>
2022-06-13 13:53:02 +0300

    Updated ChangePasswordDto

--- src/main/java/com/softarex/domas/questionnaire_portal/dto/ChangePasswordDto.java
+++ src/main/java/com/softarex/domas/questionnaire_portal/dto/ChangePasswordDto.java
@@ -11,15 +11,15 @@ import javax.validation.constraints.Size;
 @Getter
 public class ChangePasswordDto {
 
-        @Size(max = 40, min = 8, message = MessageErrorConstant.MESSAGE_INCORRECT_FORMAT_OF_CURRENT_PASSWORD)
-        @NotBlank(message = MessageErrorConstant.MESSAGE_NOT_BLANK_CURRENT_PASSWORD)
-        private String currentPassword;
+    @Size(max = 40, min = 8, message = MessageErrorConstant.MESSAGE_INCORRECT_FORMAT_OF_CURRENT_PASSWORD)
+    @NotBlank(message = MessageErrorConstant.MESSAGE_NOT_BLANK_CURRENT_PASSWORD)
+    private String currentPassword;
 
-        @Size(max = 40, min = 8, message = MessageErrorConstant.MESSAGE_INCORRECT_FORMAT_OF_NEW_PASSWORD)
-        @NotBlank(message = MessageErrorConstant.MESSAGE_NOT_BLANK_NEW_PASSWORD)
-        private String newPassword;
+    @Size(max = 40, min = 8, message = MessageErrorConstant.MESSAGE_INCORRECT_FORMAT_OF_NEW_PASSWORD)
+    @NotBlank(message = MessageErrorConstant.MESSAGE_NOT_BLANK_NEW_PASSWORD)
+    private String newPassword;
 
-    }
+}
 
 
 
